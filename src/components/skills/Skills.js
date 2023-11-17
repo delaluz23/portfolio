8870234eb100c154c85019a6c26463a088ef8a75
Jose De La Luz <Jdelaluz01@gmail.com>
2023-11-16 22:48:40 -0600

    began to make skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,10 +1,46 @@
 import React from "react";
 import 'skillsCSS.css';
+// import images for bars when ready
+
 
 const Skills = () => {
     return (
         <section id='skills'>
-            about section
+            <span className='skill title'>
+                What I Do
+            </span>
+            <span className='skill description'>
+                I BE DOING IT
+            </span>
+            <div className='skillBars'>
+                <div className='skillBar'>
+                    <img src="" alt="" className='skillbarImage'/>
+                    <h2>
+
+                    </h2>
+                    <p>
+
+                    </p>
+                </div>
+                <div className='skillBar'>
+                    <img src="" alt="" className='skillbarImage'/>
+                    <h2>
+
+                    </h2>
+                    <p>
+
+                    </p>
+                </div>
+                <div className='skillBar'>
+                    <img src="" alt="" className='skillbarImage'/>
+                    <h2>
+
+                    </h2>
+                    <p>
+
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
